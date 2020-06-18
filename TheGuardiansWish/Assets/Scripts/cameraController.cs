3377b411a931a6f067c9ca1615f6e94f2b8cd9d0
JessisCodeDesaster <jessica.truemper@gmail.com>
2020-06-18 16:42:16 +0200

    Einfacher PlayerController und CameraController

---
+++ TheGuardiansWish/Assets/Scripts/cameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1.5f;
+    public Transform viewDirection;
+    public Transform Player;
+    float mousePosX;
+    float mousePosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mousePosY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        /* -- um Achsen zu invertieren
+         mousePosX -= Input.GetAxis("Mouse X") * rotationSpeed;
+        mousePosY += Input.GetAxis("Mouse Y") * rotationSpeed;
+         */
+
+        mousePosY = Mathf.Clamp(mousePosY, -35, 55);
+
+        transform.LookAt(viewDirection);
+
+        viewDirection.rotation = Quaternion.Euler(mousePosY, mousePosX, 0);
+        Player.rotation = Quaternion.Euler(0, mousePosX, 0);
+    }
+}
---
+++ TheGuardiansWish/Assets/Scripts/characterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody body;
+    public bool playerOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized * moveSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+
+        if (Input.GetButtonDown("Jump") && playerOnGround == true)
+        {
+            body.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            playerOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            playerOnGround = true;
+        }
+    }
+}
