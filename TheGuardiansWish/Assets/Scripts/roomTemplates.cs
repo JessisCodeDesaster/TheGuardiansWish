674c435c744bcf896e4941a47c3dfb1b947950ae
JessisCodeDesaster <jessica.truemper@gmail.com>
2020-06-24 20:42:33 +0200

    Gegner-Dummies im Dungeon
    Gegner-Dummies spawnen random in den Räumen; letzter generierter Raum ist Bossraum.
    
    Verändertes Skript: roomTemplates.cs

--- TheGuardiansWish/Assets/Scripts/roomTemplates.cs
+++ TheGuardiansWish/Assets/Scripts/roomTemplates.cs
@@ -12,28 +12,56 @@ public class roomTemplates : MonoBehaviour
     public List<GameObject> rooms;
 
     public float waitTime;
-    private bool bossSpawned;
     public GameObject boss;
+    public GameObject  monster;
+
+    public int rand;
+    public int x;
+    public int z;
+    public Vector3 position;
+
+    public bool spawned;
+    int roomCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawned = false;
+        waitTime = 1;
+        roomCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(waitTime <= 0 && bossSpawned == false)
+        if(waitTime <= 0 && spawned == false)
         {
-            for (int i = 0; i < rooms.Count; i++)
-            {
-                Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
-                bossSpawned = true;
-            }
+            spawnEnemies();
         } else
         {
             waitTime -= Time.deltaTime;
         }
+    }
+
+    void spawnEnemies()
+    {
+        for (int i = 0; i < rooms.Count-1; i++)
+        {
+            rand = Random.Range(1, 5);
+            roomCount++;
+            for (int j = 0; j < rand; j++)
+            {
+                x = Random.Range(-9, 9);
+                z = Random.Range(-9, 9);
+                position = new Vector3(x, 1.5f, z);
+                Instantiate(monster, rooms[i].transform.position + position, Quaternion.identity);
+            }
+        }
+        if (roomCount+1 == rooms.Count)
+        {
+            position = new Vector3(0, 1.5f, 0);
+            Instantiate(boss, rooms[roomCount].transform.position + position, Quaternion.identity);
+        }
+        spawned = true;
     }
 }
