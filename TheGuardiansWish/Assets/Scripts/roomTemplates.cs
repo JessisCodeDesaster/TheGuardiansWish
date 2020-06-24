ea8ec29fc07b591060623f7c34754cb3320697f2
JessisCodeDesaster <jessica.truemper@gmail.com>
2020-06-24 19:42:45 +0200

    Minimap-Prototyp

--- TheGuardiansWish/Assets/Scripts/roomTemplates.cs
+++ TheGuardiansWish/Assets/Scripts/roomTemplates.cs
@@ -9,6 +9,12 @@ public class roomTemplates : MonoBehaviour
     public GameObject[] left;
     public GameObject[] right;
 
+    public List<GameObject> rooms;
+
+    public float waitTime;
+    private bool bossSpawned;
+    public GameObject boss;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +24,16 @@ public class roomTemplates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(waitTime <= 0 && bossSpawned == false)
+        {
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                bossSpawned = true;
+            }
+        } else
+        {
+            waitTime -= Time.deltaTime;
+        }
     }
 }
---
+++ TheGuardiansWish/Assets/Scripts/roomsToList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomsToList : MonoBehaviour { 
+
+    private roomTemplates templates;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<roomTemplates>();
+        templates.rooms.Add(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
